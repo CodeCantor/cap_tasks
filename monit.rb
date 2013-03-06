a67438125c0f3dfd7ca4e5bd93d527a780aaba52
Enrique Alvarez <enriqueaf@gmail.com>
2013-03-06 17:01:15 +0100

    Change the name of the monit config file for the server

--- monit.rb
+++ monit.rb
@@ -9,7 +9,7 @@ namespace :monit do
   end
   after "deploy:setup", "monit:setup"
 
-  task(:unicorn, roles: :app) { monit_config "unicorn" }
+  task(:unicorn, roles: :app) { monit_config "unicorn", "#{application}_unicorn" }
 
   %w[start stop restart syntax reload].each do |command|
     desc "Run Monit #{command} script"
@@ -21,11 +21,12 @@ namespace :monit do
   end
 end
 
-def monit_config(name, destination = nil)
-  destination ||= "/etc/monit/conf.d/#{name}.conf"
-  template "monit/#{name}.erb", "/tmp/monit_#{name}"
+def monit_config(name, d_name=nil, destination = nil)
+  d_name ||= name
+  destination ||= "/etc/monit/conf.d/#{d_name}.conf"
+  template "monit/#{name}.erb", "/tmp/monit_#{d_name}"
   with_sudo_user do
-    sudo "mv /tmp/monit_#{name} #{destination}"
+    sudo "mv /tmp/monit_#{d_name} #{destination}"
     sudo "chown root #{destination}"
     sudo "chmod 600 #{destination}"
   end
